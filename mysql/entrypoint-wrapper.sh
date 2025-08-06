7fa5c814564d7cdfdf1988342b7c702cacb16682
msivakumarreddy <info@joindevops.com>
2025-08-07 01:59:03 +0530

    terraform

--- mysql/entrypoint-wrapper.sh
+++ mysql/entrypoint-wrapper.sh
@@ -5,6 +5,7 @@ if [ -f /tmp/mysql/password.txt ]; then
   export MYSQL_ROOT_PASSWORD=$(cat /tmp/mysql/password.txt)
 fi
 
+echo "custom entrypoint started"
 # Now call the original MySQL entrypoint
 exec  /entrypoint.sh mysqld
 
